b65ad67f10622529c9cd998c1db1e2699a60b574
IRBeshlian <68671329+IRBeshlian@users.noreply.github.com>
2022-08-25 20:05:50 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Authors: Isabella Beshlian, Benny Ho
+ * Date: 2022/08/25
+ *
+ * insert purpose here lasjfkashldfk,s
+ *
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+  printf("Isabella Beshlian\n");
+
+  return 0;
+}
